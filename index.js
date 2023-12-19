c2a8e3b86cb73038a9d66aa493cdf7c0b5fc96c7
khanhdnk <121726197+khanhdnk@users.noreply.github.com>
2023-12-20 00:12:56 +0700

    update

--- index.js
+++ index.js
@@ -124,5 +124,5 @@ app.delete('/api/employees/delete/:id', (req, res) => {
 //     data: null
 //   }))
 // });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log('Server listening on port 3000'));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
